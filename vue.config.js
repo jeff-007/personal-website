da96c3169939969c352e3cb661a59dfa087c8f00
shanmao <wjs6298582@163.com>
2022-04-13 10:13:09 +0800

    项目初始配置更新

---
+++ vue.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+const port = process.env.port || process.env.npm_config_port || 9520 // dev port
+
+module.exports = {
+  /**
+   * You will need to set publicPath if you plan to deploy your site under a sub path,
+   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+   * then publicPath should be set to "/bar/".
+   * In most cases please use '/' !!!
+   * Detail: https://cli.vuejs.org/config/#publicpath
+   */
+  publicPath: process.env.NODE_ENV === 'development' ? './' : './',
+  lintOnSave: false,
+  outputDir: '../axios_demo/pack_demo',
+  assetsDir: 'static',
+  indexPath: 'index.html',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://project.moja.vip',
+    //     changeOrigin: true,
+    //     ws: true,
+    //     pathRewrite: { '^/api': '' }
+    //   },
+    //   '/m': {
+    //     target: 'http://testlocal.wmnetwork.cc',
+    //     changeOrigin: true,
+    //     pathRewrite: {'^/m': ''}
+    //   }
+    // }
+    // proxy: 'http://testlocal.wmnetwork.cc'
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  chainWebpack: config => {
+    // GraphQL Loader
+    config.module
+      .rule('worker-loader')
+      .test(/\.worker\.js$/)
+      .use('worker-loader')
+      .loader('worker-loader')
+      .end()
+  },
+  css: {
+    extract: true,
+    sourceMap: false,
+    loaderOptions: {
+      scss: {
+        // 在 sass-loader v8 中，这个选项名是 "prependData"
+        additionalData: '@import "~@/styles/variables.scss";'
+      }
+    }
+  }
+}
