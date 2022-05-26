e56cf1a36297d8eb23a2de47b63fdb6f5c8ef822
shanmao <wjs6298582@163.com>
2022-05-26 17:54:55 +0800

    增加图片裁剪demo

---
+++ src/utils/konva-crop.js
@@ -0,0 +1,255 @@
+import Konva from 'konva'
+
+/**
+ * Konva Image Cropping Plugin.
+ *
+ * cropTransform - position of the cropped image. x,y,width,height and rotation
+ * enableCropOnDblClick - enable enter cropping on dblclick
+ * cropEnd - exit cropping
+ * cropStart - enter cropping
+ */
+(function (Konva) {
+  /**
+   * Decomposes standard 2x2 matrix into transform componentes
+   * @static
+   * @memberOf geometry
+   * @param  {Array} a transformMatrix
+   * @return {Object} Components of transform
+   */
+  function qrDecompose (a) {
+    const angle = Math.atan2(a[1], a[0])
+    const denom = Math.pow(a[0], 2) + Math.pow(a[1], 2)
+    const scaleX = Math.sqrt(denom)
+    const scaleY = (a[0] * a[3] - a[2] * a[1]) / scaleX
+
+    return {
+      rotation: angle / (Math.PI / 180),
+      scaleX: scaleX,
+      scaleY: scaleY,
+      x: a[4],
+      y: a[5]
+    }
+  }
+
+  function getGroupCoords (object, group) {
+    const mB = object.getAbsoluteTransform().getMatrix()
+    const mX = group.getAbsoluteTransform().getMatrix()
+
+    // possible to replace with mB * mX.invert()
+    const M = mB[0]; const N = mB[1]; const O = mB[2]; const P = mB[3]; const R = mB[4]; const S = mB[5]
+    const A = mX[0]; const B = mX[1]; const C = mX[2]; const D = mX[3]; const E = mX[4]; const F = mX[5]
+    const AD = A * D
+    const BC = B * C
+    const G = (C * N - M * D) / (BC - AD)
+    const H = (A * N - M * B) / (AD - BC)
+    const I = (C * P - O * D) / (BC - AD)
+    const J = (A * P - O * B) / (AD - BC)
+    const K = (C * (S - F) + D * (E - R)) / (BC - AD)
+    const L = (A * (S - F) + B * (E - R)) / (AD - BC)
+
+    const matrix = [G, H, I, J, K, L]
+    const options = qrDecompose(matrix)
+
+    return options
+  }
+
+  /**
+   * enable Cropping On DblClick
+   */
+  Konva.Image.prototype.enableCropOnDblClick = function () {
+    this.on('dblclick', function (e) {
+      this.cropStart()
+    })
+  }
+
+  Konva.Image.prototype.setCropTransform = function (value) {
+    if (value === false) {
+      delete this._cropElement
+      return
+    }
+    if (!this._cropElement) {
+      this._cropElement = new Konva.Shape()
+    }
+    this._cropElement.setAttrs(value)
+    this._cropElement.setAttrs({
+      offsetX: 0,
+      offsetY: 0
+    })
+  }
+
+  Konva.Image.prototype.getCropTransform = function () {
+    return this._cropElement && this._cropElement.getAttrs()
+  }
+
+  Konva.Image.prototype.cropTransform = function (value) {
+    if (value) {
+      this.setCropTransform(value)
+    } else {
+      return this.getCropTransform()
+    }
+  }
+
+  Konva.Image.prototype.cropEnd = function (context) {
+    if (this.cropImage) {
+      this.transformer.destroy()
+      this.cropImageTransformer.destroy()
+      this.cropImage.off('dragmove', this.cropUpdateBinded)
+      this.cropImage.off('transform', this.cropUpdateBinded)
+      this.off('dragmove', this.cropUpdateBinded)
+      this.off('transform', this.resizAndCropUpdateBinded)
+      this.cropImage.remove()
+      delete this.cropImage
+      delete this.transformer
+      delete this.cropImageTransformer
+      this.getLayer().draw()
+    }
+  }
+
+  Konva.Image.prototype.cropUpdate = function (context) {
+    const options = getGroupCoords(this.cropImage, this)
+    this.cropTransform(options)
+    this.getLayer().draw()
+  }
+
+  Konva.Image.prototype.resize = function () {
+    this.setAttrs({
+      scaleX: 1,
+      scaleY: 1,
+      width: this.width() * this.scaleX(),
+      height: this.height() * this.scaleY()
+    })
+  }
+
+  Konva.Image.prototype.cropReset = function (context) {
+    if (this.cropImage) {
+      this.cropEnd()
+    }
+    this.setCropTransform(false)
+    this.getLayer().draw()
+  }
+
+  Konva.Image.prototype.cropStart = function (context) {
+    this.getStage().find('Transformer').destroy()
+
+    if (this.cropImage) {
+      return
+    }
+    if (!this._cropElement) {
+      this.cropTransform({
+        x: 0,
+        y: 0,
+        width: this.width(),
+        height: this.height(),
+        rotation: 0
+      })
+    }
+    const layer = this.getLayer()
+    const transform = this.getAbsoluteTransform()
+    const transform2 = this._cropElement.getAbsoluteTransform()
+    const transform0 = layer.getAbsoluteTransform()
+    const options = qrDecompose(transform0.copy().invert().multiply(transform).multiply(transform2).getMatrix())
+
+    this.cropImage = new Konva.Image({
+      stroke: this.stroke(),
+      strokeWidth: this.strokeWidth(),
+      image: this.image(),
+      opacity: 0.5,
+      draggable: true
+    })
+    this.cropImage.isCroppingElement = true
+    this.cropImage.setAttrs(options)
+    this.cropImage.setAttrs({
+      width: this._cropElement.width(),
+      height: this._cropElement.height()
+    })
+
+    layer.add(this.cropImage)
+    this.cropImageTransformer = new Konva.Transformer({
+      borderDash: [5, 5],
+      anchorSize: 21,
+      anchorCornerRadius: 11
+    })
+      .attachTo(this.cropImage)
+
+    this.transformer = new Konva.Transformer()
+      .attachTo(this)
+
+    layer.add(this.cropImageTransformer, this.transformer)
+
+    this.cropUpdateBinded = this.cropUpdate.bind(this)
+
+    this.resizAndCropUpdateBinded = function () {
+      this.resize()
+      this.cropUpdate()
+    }.bind(this)
+
+    this.resize()
+    this.cropUpdate()
+    this.cropImage.on('dragmove', this.cropUpdateBinded)
+    this.cropImage.on('transform', this.cropUpdateBinded)
+    this.on('dragmove', this.cropUpdateBinded)
+    this.on('transform', this.resizAndCropUpdateBinded)
+
+    this.getStage().on('click tap', (e) => {
+      if (e.target !== this.cropImage && e.target !== this) {
+        this.cropEnd()
+      }
+    })
+    layer.draw()
+  }
+
+  Konva.Image.prototype._sceneFunc = function (context) {
+    let width = this.width()
+    let height = this.height()
+    const image = this.image()
+    let cropWidth
+    let cropHeight
+    let params
+
+    context.save()
+    context.beginPath()
+    context.rect(0, 0, width, height)
+    context.closePath()
+    context.clip()
+    if (this.hasFill() || this.hasStroke()) {
+      context.fillStrokeShape(this)
+    }
+
+    if (image) {
+      if (this._cropElement) {
+        context.save()
+        width = this._cropElement.width()
+        height = this._cropElement.height()
+        const transform = this._cropElement.getAbsoluteTransform()
+        const m = transform.getMatrix()
+        context.transform(m[0], m[1], m[2], m[3], m[4], m[5])
+      }
+
+      cropWidth = this.cropWidth()
+      cropHeight = this.cropHeight()
+      if (cropWidth && cropHeight) {
+        params = [
+          image,
+          this.cropX(),
+          this.cropY(),
+          cropWidth,
+          cropHeight,
+          0,
+          0,
+          width,
+          height
+        ]
+      } else {
+        params = [image, 0, 0, width, height]
+      }
+
+      context.drawImage.apply(context, params)
+
+      if (this._cropElement) {
+        context.restore()
+      }
+    }
+    context.strokeShape(this)
+    context.restore()
+  }
+})(Konva)
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,158 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <button id="cropPhotoEnd" @click="cropEndHandle">cropPhotoEnd</button>
+    <button id="cropPhotoStart" @click="cropStartHandle">cropPhotoStart</button>
+    <button id="cropPhotoReset" @click="cropResetHandle">cropPhotoReset</button>
+    <div id="container"></div>
   </div>
 </template>
+
+<script>
+import Konva from 'konva'
+// import konvaCrop from '@/utils/konva-crop.js'
+export default {
+  name: 'Home',
+  components: {},
+  data () {
+    return {
+      stage: null,
+      layer: null,
+      transformer: null,
+      target: null,
+      defaultImg: 'https://img2.wmnetwork.cc/w/202112/09/0_20211209165113_keQSPZNW.png'
+    }
+  },
+  created () {},
+  mounted () {
+    console.log('Konva', Konva)
+    this.initStage()
+    this.initImg(this.defaultImg)
+  },
+  methods: {
+    cropEndHandle () {
+      console.log(this.target)
+      this.target && this.target.cropEnd()
+    },
+    cropStartHandle () {
+      console.log(this.target)
+      this.target && this.target.cropStart()
+    },
+    cropResetHandle () {
+      console.log(this.target)
+      this.target && this.target.cropReset()
+    },
+    initStage () {
+      this.stage = new Konva.Stage({
+        container: 'container',
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+      this.layer = new Konva.Layer()
+      this.stage.add(this.layer)
+
+      this.stage.on('click tap', (e) => {
+        if (this.transformer) {
+          this.transformer.destroy()
+        }
+        if (e.target === this.stage || e.target.isCroppingElement) {
+          this.layer.draw()
+          return
+        }
+        this.target = e.target
+        console.log('target', this.target)
+        this.transformer = new Konva.Transformer()
+        this.layer.add(this.transformer)
+        this.transformer.nodes([e.target])
+        this.layer.batchDraw()
+      })
+      this.stage.scaleX(2)
+      this.stage.scaleY(2)
+    },
+
+    // 初始化图片
+    initImg (src) {
+      Konva.Image.fromURL(src, (konvaImage) => {
+        konvaImage.setAttrs({
+          stroke: 'black',
+          strokeWidth: 1,
+          y: 100,
+          x: 118,
+          width: 200,
+          height: 125,
+          draggable: true,
+          cropTransform: {
+            height: 175,
+            rotation: 10,
+            scaleX: 1.256718546954032,
+            scaleY: 1.6075454233616444,
+            width: 250,
+            x: -23.179330196090106,
+            y: -100.75893930126733
+          }
+        })
+        // konvaImage.enableCropOnDblClick()
+        this.layer.add(konvaImage)
+        this.layer.draw()
+        // setTimeout(() => {
+        //   konvaImage.cropStart()
+        // })
+      })
+
+      Konva.Image.fromURL(src, (konvaImage) => {
+        konvaImage.setAttrs({
+          y: 50,
+          x: 350,
+          width: 300,
+          height: 300,
+          draggable: true,
+          clip: {
+            x: 100,
+            y: 40,
+            width: 10,
+            height: 20
+          }
+        })
+        // konvaImage.enableCropOnDblClick()
+        this.layer.add(konvaImage)
+        this.layer.draw()
+      })
+    }
+  }
+}
+
+/**
+   * Konva Image Cropping Plugin.
+   *
+   * cropTransform - position of the cropped image. x,y,width,height and rotation
+   * enableCropOnDblClick - enable enter cropping on dblclick
+   * cropEnd - exit cropping
+   * cropStart - enter cropping
+   */
+
+</script>
+
+<style scoped lang="scss">
+  html,body,#container{
+    width: 100%;
+    height: 100%;
+    margin: 0;
+  }
+
+  button{
+    position: absolute;
+    top: 10px;
+    width: 120px;
+    box-sizing: border-box;
+    line-height:30px;
+    z-index: 1
+  }
+  #cropPhotoEnd{
+    left:10px;
+  }
+  #cropPhotoStart{
+    left:140px;
+  }
+  #cropPhotoReset{
+    left:270px;
+  }
+</style>
